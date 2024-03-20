cfa705b4e80e942ea92e51d5b1c29beee46ed2d4
AMAN DATTA <aman.datta@colortokens.com>
2024-03-20 13:53:54 +0530

    the parity program

--- go/parity.go
+++ go/parity.go
@@ -6,6 +6,22 @@ import (
 )
 
 func Parity() {
-	var x uint64 = 125
-	fmt.Println(strconv.FormatUint(x, 2))
+	var xx [5]uint64 = [5]uint64{125, 11, 20, 24, 13}
+	for _, x := range xx {
+		fmt.Println(strconv.FormatUint(x, 2))
+		x ^= x >> 32
+		x ^= x >> 16
+		x ^= x >> 8
+		x ^= x >> 4
+		x ^= x >> 2
+		x ^= x >> 1
+
+		fmt.Println(x & 1)
+		if x&1 == 1 {
+			fmt.Println("Odd")
+		} else {
+			fmt.Println("Even")
+		}
+	}
+
 }
